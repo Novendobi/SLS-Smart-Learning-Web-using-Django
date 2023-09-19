51c59fb2cf48ad595e9c70537c6940bfc56f256f
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-09-19 23:31:33 +0500

    ok

--- smartlearning/settings.py
+++ smartlearning/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,6 +157,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 #User Upload the contetns. images files
 MEDIA_URL = '/images/'
