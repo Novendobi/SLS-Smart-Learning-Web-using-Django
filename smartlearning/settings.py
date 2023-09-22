0e60271c65f934ddbea249c60ce47e092a1bf2b4
Novendobi <nchidinma4@gmail.com>
2023-09-22 08:25:46 -0400

    csrfcomm

--- smartlearning/settings.py
+++ smartlearning/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = [ ]
